7b6bcf0a087b4a881d5d4c075f6eb812daad3162
Joseph Wu <josephwu0507@gmail.com>
2024-10-01 17:31:07 -0400

    Completed part 2

--- src/hw2.c
+++ src/hw2.c
@@ -81,8 +81,61 @@ void print_packet(unsigned int packet[])
 
 void store_values(unsigned int packets[], char *memory)
 {
-    (void)packets;
-    (void)memory;
+    unsigned int * iterator = packets;
+    while( (*iterator >> 10) == 1048576){
+        int length = (*iterator) & 1023;
+        iterator ++;
+        unsigned int BE_mask = 15;
+        int firstBE = (*iterator) & BE_mask;
+        int lastBE = (*iterator >> 4) & BE_mask;
+        iterator ++;
+        int address = (*iterator) | 0;
+        for(int row =0 ; row < length; row ++){
+            iterator++;
+            if(row == 0){
+                int firstBE_0 = (firstBE & 1);
+                int firstBE_1 = (firstBE & 1 << 1);
+                int firstBE_2 = (firstBE & 1 << 2);
+                int firstBE_3 = (firstBE & 1 << 3);
+
+                if(firstBE_0){
+                    memory[address + (row*4)] = (*iterator) & 511;
+                }
+                if(firstBE_1){
+                    memory[address + 1 + (row*4)] = (*iterator >> (8*1)) & 511;
+                }
+                if(firstBE_2){
+                    memory[address + 2 + (row*4)] = (*iterator >> (8*2)) & 511;
+                }
+                if(firstBE_3){
+                    memory[address + 3 + (row*4)] = (*iterator >> (8*3)) & 511;
+                }
+            } else if(row == length-1){
+                int lastBE_0 = (lastBE & 1);
+                int lastBE_1 = (lastBE & 1 << 1);
+                int lastBE_2 = (lastBE & 1 << 2);
+                int lastBE_3 = (lastBE & 1 << 3);
+
+                if(lastBE_0){
+                    memory[address + (row*4)] = (*iterator) & 511;
+                }
+                if(lastBE_1){
+                    memory[address + 1 + (row*4)] = (*iterator >> (8*1)) & 511;
+                }
+                if(lastBE_2){
+                    memory[address + 2 + (row*4)] = (*iterator >> (8*2)) & 511;
+                }
+                if(lastBE_3){
+                    memory[address + 3 + (row*4)] = (*iterator >> (8*3)) & 511;
+                }
+            } else{
+                for(int i = 0; i<4; i++){
+                    memory[address + i + (row*4)] = (*iterator >> (8*i)) & 511;
+                }
+            }
+        }
+        iterator ++;
+    }
 }
 
 unsigned int* create_completion(unsigned int packets[], const char *memory)
